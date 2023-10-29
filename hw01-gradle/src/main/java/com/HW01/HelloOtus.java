3460c7285f00ddda7c4857c2c48865e2679c5d92
LohmatiyShmel <131897080+LohmatiyShmel@users.noreply.github.com>
2023-10-29 14:33:00 +0300

    2
    2

---
+++ hw01-gradle/src/main/java/com/HW01/HelloOtus.java
@@ -0,0 +1,11 @@
+package com.HW01;
+import com.google.common.collect.Lists;
+import java.util.List;
+public class HelloOtus {
+    public static void main(String[] args) {
+        List<String> fruitList = Lists.newArrayList("Apple", "Orange", "Banana");
+        for(String fruit : fruitList) {
+            System.out.println(fruit);
+        }
+    }
+}
